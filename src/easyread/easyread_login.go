1e5858f21b8f1eda490f3b8282e9beb03a45be0f
NoahShen <Noah.Shen87@gmail.com>
2013-09-14 19:15:00 +0800

    add get subsummary code

--- src/easyread/easyread_login.go
+++ src/easyread/easyread_login.go
@@ -2,21 +2,31 @@ package easyread
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
-	//"net/url"
-	"io"
 	"strings"
+	"time"
 	"utils"
 )
 
 const (
-	LOING_URL = "https://easyread.163.com/sns/login/login.atom"
+	LOGIN_URL          = "https://easyread.163.com/sns/login/login.atom"
+	GET_SUBSUMMARY_URL = "http://easyread.163.com/user/subsummary.atom?rand=%d"
 )
 
+type SubSummary struct {
+	Id   string
+	Name string
+	Type string
+}
+
 type EasyreadSession struct {
-	cookies []*http.Cookie
+	UserId   string
+	Username string
+	cookies  []*http.Cookie
 }
 
 func (self *EasyreadSession) login(username, password string) error {
@@ -26,7 +36,7 @@ func (self *EasyreadSession) login(username, password string) error {
 	loginInfo["username"] = username
 	loginInfoJson, _ := json.Marshal(loginInfo)
 	b := strings.NewReader(string(loginInfoJson))
-	req := self.createHttpRequest("POST", "application/json", LOING_URL, b)
+	req := self.createHttpRequest("POST", LOGIN_URL, "application/json", b)
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
@@ -35,13 +45,42 @@ func (self *EasyreadSession) login(username, password string) error {
 	if respErr != nil {
 		return respErr
 	}
-	fmt.Println("content:", string(content))
+	loginResult := make(map[string]interface{})
+	unmarshalErr := json.Unmarshal(content, &loginResult)
+	if unmarshalErr != nil {
+		return unmarshalErr
+	}
+	resCode := loginResult["resCode"].(float64)
+	if resCode != 0 {
+		return errors.New("resCode is not zero!")
+	}
+	userInfo := loginResult["userInfo"].(map[string]interface{})
+	self.Username = userInfo["urs"].(string)
+	self.UserId = userInfo["userId"].(string)
 	return nil
 }
 
-func (self *EasyreadSession) createHttpRequest(method, contentType, url string, body io.Reader) *http.Request {
+func (self *EasyreadSession) getSubSummary() ([]SubSummary, error) {
+	subsummaries := make([]SubSummary, 0)
+	url := fmt.Sprintf(GET_SUBSUMMARY_URL, time.Now().UTC().Unix())
+	req := self.createHttpRequest("GET", url, "", nil)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return subsummaries, err
+	}
+	content, respErr := self.getResponseContent(resp)
+	if respErr != nil {
+		return subsummaries, respErr
+	}
+	fmt.Println("content:", string(content))
+	return subsummaries, nil
+}
+
+func (self *EasyreadSession) createHttpRequest(method, url, contentType string, body io.Reader) *http.Request {
 	req, _ := http.NewRequest(method, url, body)
-	req.Header.Set("Content-Type", contentType)
+	if len(contentType) > 0 {
+		req.Header.Set("Content-Type", contentType)
+	}
 	req.Header.Set("User-Agent", "Pris/3.0.0")
 	req.Header.Set("X-User-Agent", "PRIS/3.0.0 (768/1184; android; 4.3; zh-CN; android) 1.2.8")
 	req.Header.Set("X-Pid", "(000000000000000;d41d8cd98f00b204e9800998ecf8427e;)")
--- src/easyread/easyread_test.go
+++ src/easyread/easyread_test.go
@@ -1,13 +1,25 @@
 package easyread
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestEasyLogin(t *testing.T) {
+func _TestEasyLogin(t *testing.T) {
 	session := &EasyreadSession{}
 	err := session.login("username", "password")
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestGetSubSummary(t *testing.T) {
+	session := &EasyreadSession{}
+	err := session.login("username", "password")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println("userId:", session.UserId)
+	fmt.Println("username:", session.Username)
+	session.getSubSummary()
 }
