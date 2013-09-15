a3bbc09bc568a44aab9eee6b64258c47f542ec2a
NoahShen <Noah.Shen87@gmail.com>
2013-09-15 22:23:43 +0800

    getting subsummary source

--- src/easyread/easyread_login.go
+++ src/easyread/easyread_login.go
@@ -14,8 +14,9 @@ import (
 )
 
 const (
-	LOGIN_URL          = "https://easyread.163.com/sns/login/login.atom"
-	GET_SUBSUMMARY_URL = "http://easyread.163.com/user/subsummary.atom?rand=%d"
+	LOGIN_URL                 = "https://easyread.163.com/sns/login/login.atom"
+	GET_SUBSUMMARY_URL        = "http://easyread.163.com/user/subsummary.atom?rand=%d"
+	GET_SUBSUMMARY_SOURCE_URL = "http://easyread.163.com/news/source/index.atom?id=%s"
 )
 
 type SubSummary struct {
@@ -36,13 +37,36 @@ type EntryStatus struct {
 	Style   string   `xml:"style,attr"`
 }
 
+type NewsFeed struct {
+	XMLName     xml.Name    `xml:"feed"`
+	Id          string      `xml:"id"`
+	Title       string      `xml:"title"`
+	UpdatedDate string      `xml:"updated"`
+	Entries     []NewsEntry `xml:"entry"`
+}
+
+type NewsEntry struct {
+	XMLName      xml.Name     `xml:"entry"`
+	Id           string       `xml:"id"`
+	Title        string       `xml:"title"`
+	Author       string       `xml:"author>name"`
+	UpdatedDate  string       `xml:"updated"`
+	EntryContent EntryContent `xml:"content"`
+}
+
+type EntryContent struct {
+	XMLName     xml.Name `xml:"content"`
+	Content     string   `xml:",chardata"`
+	ContentType string   `xml:"type,attr"`
+}
+
 type EasyreadSession struct {
 	UserId   string
 	Username string
 	cookies  []*http.Cookie
 }
 
-func (self *EasyreadSession) login(username, password string) error {
+func (self *EasyreadSession) Login(username, password string) error {
 	loginInfo := make(map[string]interface{})
 	loginInfo["accountType"] = 0
 	loginInfo["auth"] = utils.MD5Encode(password)
@@ -70,10 +94,33 @@ func (self *EasyreadSession) login(username, password string) error {
 	userInfo := loginResult["userInfo"].(map[string]interface{})
 	self.Username = userInfo["urs"].(string)
 	self.UserId = userInfo["userId"].(string)
+
+	cookies := resp.Cookies()
+	self.cookies = cookies
+
 	return nil
 }
 
-func (self *EasyreadSession) getSubSummary() (SubSummary, error) {
+func (self *EasyreadSession) GetNewsSource(summaryEntryId string) (NewsFeed, error) {
+	var newsFeed = NewsFeed{}
+	url := fmt.Sprintf(GET_SUBSUMMARY_SOURCE_URL, summaryEntryId)
+	req := self.createHttpRequest("GET", url, "", nil)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return newsFeed, err
+	}
+	content, respErr := self.getResponseContent(resp)
+	if respErr != nil {
+		return newsFeed, respErr
+	}
+	unmarshalErr := xml.Unmarshal(content, &newsFeed)
+	if unmarshalErr != nil {
+		return newsFeed, unmarshalErr
+	}
+	return newsFeed, nil
+}
+
+func (self *EasyreadSession) GetSubSummary() (SubSummary, error) {
 	var subSummar = SubSummary{}
 	url := fmt.Sprintf(GET_SUBSUMMARY_URL, time.Now().UTC().Unix())
 	req := self.createHttpRequest("GET", url, "", nil)
@@ -108,7 +155,5 @@ func (self *EasyreadSession) createHttpRequest(method, url, contentType string, 
 
 func (self *EasyreadSession) getResponseContent(resp *http.Response) ([]byte, error) {
 	defer resp.Body.Close()
-	cookies := resp.Cookies()
-	self.cookies = cookies
 	return ioutil.ReadAll(resp.Body)
 }
--- src/easyread/easyread_test.go
+++ src/easyread/easyread_test.go
@@ -7,7 +7,7 @@ import (
 
 func _TestEasyLogin(t *testing.T) {
 	session := &EasyreadSession{}
-	err := session.login("username", "password")
+	err := session.Login("username", "password")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -15,15 +15,25 @@ func _TestEasyLogin(t *testing.T) {
 
 func TestGetSubSummary(t *testing.T) {
 	session := &EasyreadSession{}
-	err := session.login("username", "password")
+	err := session.Login("username", "password")
 	if err != nil {
 		t.Fatal(err)
 	}
 	fmt.Println("userId:", session.UserId)
 	fmt.Println("username:", session.Username)
-	subSummary, getSummaryErr := session.getSubSummary()
+	subSummary, getSummaryErr := session.GetSubSummary()
 	if getSummaryErr != nil {
 		t.Fatal(getSummaryErr)
 	}
-	fmt.Printf("subsummary: %v", subSummary)
+	fmt.Printf("subsummary: %v\n", subSummary)
+	fmt.Println("============")
+
+	newsFeed, newsSourceErr := session.GetNewsSource(subSummary.Entries[0].Id)
+	if newsSourceErr != nil {
+		t.Fatal(newsSourceErr)
+	}
+	for _, newsEntry := range newsFeed.Entries {
+		fmt.Printf("newsEntry: %+v\n", newsEntry)
+	}
+
 }
